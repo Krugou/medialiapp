4f3f9621b4745e47971fcf604caf9de33266243d
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-26 14:00:09 +0200

    Update pm2thekilledapp.js

--- pm2thekilledapp.js
+++ pm2thekilledapp.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [{
-    script: "backend-rest/thekilledapp.js",
-    watch: ["backend-rest"],
-    ignore_watch: ["backend-rest/uploads", "backend-rest/thumbnails"]
+    script: "./backend-rest/thekilledapp.js",
+    watch: ["./backend-rest"],
+    ignore_watch: ["./backend-rest/uploads", "./backend-rest/thumbnails"]
 
   }]
 }
