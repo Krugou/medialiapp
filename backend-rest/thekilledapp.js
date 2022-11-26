69a5d2637b3cd3c52127337210f6f3e3f66e45ea
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-26 13:38:50 +0200

    Update thekilledapp.js

--- backend-rest/thekilledapp.js
+++ backend-rest/thekilledapp.js
@@ -7,12 +7,12 @@ const https = require('https');
 const app = express();
 app.set('view engine', 'ejs');
 // //https://wiki.centos.org/HowTos/Https#head-37cd1f5c67d362756f09313cd758bef48407c325
-// const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
-// const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
-// const options = {
-//     key: sslkey,
-//     cert: sslcert
-// };
+const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+const options = {
+    key: sslkey,
+   cert: sslcert
+ };
 const date = { d: Date.now() }
    
         
@@ -24,10 +24,10 @@ app.get('/status', (req, res) => {
     
     ;
 });
-// app.get('/', (req, res) => {
-//     if (req.secure) res.send('');
-//     else res.send('pages/about');
-// });
+app.get('/', (req, res) => {
+    if (req.secure) res.send('https secure');
+    else res.send('http not secure');
+});
 
 app.listen(3000); //normal http traffic
-// https.createServer(options, app).listen(8000); //https traffic
+https.createServer(options, app).listen(8000); //https traffic
