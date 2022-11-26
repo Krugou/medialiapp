2fad2de9e1cc7b29d7e4b9915a39cd5c75714e5d
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-26 13:33:34 +0200

    Update thekilledapp.js

---
+++ backend-rest/thekilledapp.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+
+const app = express();
+app.set('view engine', 'ejs');
+// //https://wiki.centos.org/HowTos/Https#head-37cd1f5c67d362756f09313cd758bef48407c325
+// const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+// const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+// const options = {
+//     key: sslkey,
+//     cert: sslcert
+// };
+const date = { d: Date.now() }
+   
+        
+app.get('/status', (req, res) => {
+    
+    res.render('status', {
+        date: date.d
+    });
+    
+    ;
+});
+// app.get('/', (req, res) => {
+//     if (req.secure) res.send('');
+//     else res.send('pages/about');
+// });
+
+app.listen(3000); //normal http traffic
+// https.createServer(options, app).listen(8000); //https traffic
