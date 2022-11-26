311bbc24c36fa69902afcd9f81a7ebd4d0fe45a9
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-26 19:16:50 +0200

    Update thekilledapp.js

--- backend-rest/thekilledapp.js
+++ backend-rest/thekilledapp.js
@@ -19,7 +19,7 @@ const options = {
 // kommentoi yllä oleva pois että tämä toimii kotikoneella
 const date = { d: Date.now() }
 console.log(mariadbstatus)
-const mariadbstatusfixed = mariadbstatus.replace('Main').replace('--plugin-dir=/usr/lib64/mysql/plugin', ' ').replace('--log-error=/var/log/mariadb/mariadb.log', ' ').replace('--pid-file=/var/run/mariadb/mariadb.pid', ' ').replace('--socket=/var/lib/mysql/mysql.sock', ' '
+const mariadbstatusfixed = mariadbstatus.toString().replace('Main').replace('--plugin-dir=/usr/lib64/mysql/plugin', ' ').replace('--log-error=/var/log/mariadb/mariadb.log', ' ').replace('--pid-file=/var/run/mariadb/mariadb.pid', ' ').replace('--socket=/var/lib/mysql/mysql.sock', ' '
 ).replace('mariadb.service', ' ').replace('-', ' ').replace('MariaDB', ' ').replace('database', ' ').replace('server', ' ').replace('Loaded:', ' ').replace('loaded', ' ').replace('(/usr/lib/systemd/system/mariadb.service;', ' ').replace('enabled;', ' ').replace('vendor', ' ').replace('preset:', ' ').replace('disabled)', ' ')
 console.log(mariadbstatusfixed)
 
