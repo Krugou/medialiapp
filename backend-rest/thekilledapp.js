22b3a7f40e61bdcf1175e0e371747acb3ceb8a8b
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-27 15:02:13 +0200

    Update thekilledapp.js

--- backend-rest/thekilledapp.js
+++ backend-rest/thekilledapp.js
@@ -14,14 +14,15 @@ if (process.env.NODE_ENV === 'production') {
     const mariadbstatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/mariadbstatus.txt');
     const apachestatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/apachestatus.txt');
     const date = { d: Date.now() }
+    const apachestatusfixed = apachestatus.toString().replace('●', '').replace('DFOREGROUND', '');
 
-    const mariadbstatusfixed = mariadbstatus.toString().replace('Main').replace('--plugin-dir=/usr/lib64/mysql/plugin', '').replace('--log-error=/var/log/mariadb/mariadb.log', '').replace('--pid-file=/var/run/mariadb/mariadb.pid', '').replace('--socket=/var/lib/mysql/mysql.sock', ' ').replace('mariadb.service', '').replace('-', '').replace('MariaDB', '').replace('database', '').replace('server', '').replace('Loaded:', '').replace('loaded', '').replace('(/usr/lib/systemd/system/mariadb.service;', '').replace('enabled;', '').replace('vendor', '').replace('preset:', '').replace('disabled', '').replace('undefined', '').replace('PID:', '').replace('1228', '').replace('', '').replace('(mysqld_safe)', '').replace('CGroup:', '').replace('/system.slice/mariadb.service', '').replace('├─1228', '').replace('bin', '').replace('/', '').replace('sh', '').replace('usr', '').replace('bin', '').replace('mysqld_safe--basedir', '').replace('/usr', '').replace('└─1436', '').replace('usr', '').replace('libexec', '').replace('mysqld--basedir', '').replace('', '').replace('/usr', '').replace('--datadir=/var', '').replace('lib', '').replace('mysql', '').replace('/', '').replace('d_safe', '').replace('--basedir=', '').replace('/mysqld', '').replace('--basedir=', '').replace('/mysql', '')
+    const mariadbstatusfixed = mariadbstatus.toString().replace('●', '');
     app.get('/status', (req, res) => {
 
         res.render('status', {
             date: date.d,
             mariadbstatus: mariadbstatusfixed,
-            apachestatus: apachestatus,
+            apachestatus: apachestatusfixed,
         });
 
         ;
