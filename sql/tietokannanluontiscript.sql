040ea4a25459cf56501be9fe56d2348c53037fa5
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 16:47:11 +0200

    Update tietokannanluontiscript.sql

--- sql/tietokannanluontiscript.sql
+++ sql/tietokannanluontiscript.sql
@@ -151,10 +151,9 @@ CREATE USER 'jakrecreguser'@localhost IDENTIFIED BY 'xjakrecreguserx';
 
 -- user privileges
 
-grant all privileges on jakrecipes.* to 'jakrecadmin'@localhost;
-grant select, insert, update, delete on jakrecipes.* to 'jakrecreguser'@localhost;
-grant select on jakrecipes.* to 'jakrecuser'@localhost;
-
+GRANT ALL PRIVILEGES ON *.* TO 'jakrecadmin'@'localhost' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'jakrecreguser'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+GRANT SELECT ON *.* TO 'jakrecuser'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 
 
 
