8a9b9f6ac79a84f49d7f54ff0d1e6f9511bab7b2
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 21:30:13 +0200

    Update db.js

--- backend-rest/database/db.js
+++ backend-rest/database/db.js
@@ -13,33 +13,7 @@ if (process.env.NODE_ENV === 'production') {
         queueLimit: 0,
     });
 
-    var pooladmin = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER_ALL,
-        password: process.env.DB_PASS_ALL,
-        database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0,
-    });
-    var poolreguser = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER_CREATEREADUPDATEDELETE,
-        password: process.env.DB_PASS_CREATEREADUPDATEDELETE,
-        database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 10000,
-        queueLimit: 0,
-    });
-    var pooluser = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER_READ,
-        password: process.env.DB_PASS_READ,
-        database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 10000,
-        queueLimit: 0,
-    });
+  
 } else {
 
     var local = mysql.createConnection({
@@ -52,5 +26,31 @@ if (process.env.NODE_ENV === 'production') {
         queueLimit: 0,
     });
 }
-
+const pooladmin = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER_ALL,
+    password: process.env.DB_PASS_ALL,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+const poolreguser = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER_CREATEREADUPDATEDELETE,
+    password: process.env.DB_PASS_CREATEREADUPDATEDELETE,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10000,
+    queueLimit: 0,
+});
+const pooluser = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER_READ,
+    password: process.env.DB_PASS_READ,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10000,
+    queueLimit: 0,
+});
 module.exports = local, pooladmin, poolreguser, pooluser, admin;
