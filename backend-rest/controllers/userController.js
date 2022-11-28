80408ad7d9d3759b25bbbf7c7a73d454c83bf7a0
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 17:35:03 +0200

    Update userController.js

--- backend-rest/controllers/userController.js
+++ backend-rest/controllers/userController.js
@@ -1,5 +1,9 @@
 'use strict';
 
+
+const { getAllUsersAdmin, getUsersCountAdmin, user_post } = require('../models/userModel');
+const { httpError } = require('../utils/errors');
+
 const user_post = async (req, res) => {
 
     const data = [
@@ -10,9 +14,33 @@ const user_post = async (req, res) => {
 
 };
 
-
-
-
-module.exports = {
-    user_post,
+const user_list_admin_get = async (req, res, next) => {
+    try {
+        const users = await getAllUsersAdmin(next);
+        if (users.length < 1) {
+            next(httpError('No users found', 404));
+            return;
+        }
+        res.json(users);
+    } catch (e) {
+        console.error('user_list_get', e.message);
+        next(httpError('Internal server error', 500));
+    }
+};
+const user_count_admin_get = async (req, res, next) => {
+    try {
+        const count = await getUsersCountAdmin(next);
+        if (count < 1) {
+            next(httpError('No users found', 404));
+            return;
+        }
+        res.json(count);
+    } catch (e) {
+        console.error('user_count_get', e.message);
+        next(httpError('Internal server error', 500));
+    }
 }
+module.exports = {
+    user_list_admin_get,
+    user_count_admin_get, user_post,
+};
