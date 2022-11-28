9c4eb2eba6e480c8e4a1bb3b36aaf36b73caac6e
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2022-11-28 17:23:58 +0200

    rekisteröinti säädöksiä

---
+++ backend-rest/controllers/userController.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const user_post = async (req, res) => {
+
+    const data = [
+        req.body.emailInput,
+        req.body.usernameInput,
+        req.body.passwordInput,
+    ];
+
+};
+
+
+
+
+module.exports = {
+    user_post,
+}
