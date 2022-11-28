5019967c2d16c540bfc5d89ea1d3748504abf4de
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 19:56:57 +0200

    Update db.js

--- backend-rest/database/db.js
+++ backend-rest/database/db.js
@@ -17,7 +17,7 @@ const poolreguser = mysql.createPool({
     password: process.env.DB_PASS_CREATEREADUPDATEDELETE,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 10000,
     queueLimit: 0,
 });
 const pooluser = mysql.createPool({
@@ -26,7 +26,7 @@ const pooluser = mysql.createPool({
     password: process.env.DB_PASS_READ,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 10000,
     queueLimit: 0,
 });
 
