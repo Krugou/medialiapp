22ba6bbc70fa584639c7a7d2fdf7b0a7fa37c4c0
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 17:35:02 +0200

    Create userModel.js

---
+++ backend-rest/models/userModel.js
@@ -0,0 +1,33 @@
+const { poolAdmin,poolRegUser,poolUser } = require('../database/db');
+const promisePoolAdmin = poolAdmin.promise();
+// const promisePoolRegUser = poolRegUser.promise();
+// const promisePoolUser  = poolUser.promise();
+
+
+
+const getUsersCountAdmin = async (next) => {
+    try {
+        const [rows] = await promisePoolAdmin.execute('SELECT COUNT(*) AS count FROM users');
+        return rows[0].count;
+    } catch (e) {
+        console.error('getUsersCountAdmin', e.message);
+        next(httpError('Database error', 500));
+    }
+};
+
+const getAllUsersAdmin = async (next) => {
+    try {
+        const [rows] = await promisePoolAdmin.execute(`SELECT *
+                                                FROM users;
+                                                `);
+        return rows;
+    } catch (e) {
+        console.error('getAllUsersAdmin', e.message);
+        next(httpError('Database error', 500));
+    }
+};
+module.exports = {
+    getAllUsersAdmin,
+    getUsersCountAdmin,
+
+};
