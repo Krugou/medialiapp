b4959dcc1575ae996373d4cbb6130d95b7e746cf
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 20:26:27 +0200

    Update thekilledapp.js

--- backend-rest/thekilledapp.js
+++ backend-rest/thekilledapp.js
@@ -6,6 +6,8 @@ const app = express();
 const fs = require('fs');
 const userRoute = require('./routes/userRoute');
 const { httpError } = require('./utils/errors');
+const pooladmin = require('../database/db');
+const promisePoolAdmin = pooladmin.promise();
 
 
 app.set('view engine', 'ejs');
@@ -22,32 +24,43 @@ if (process.env.NODE_ENV === 'production') {
     const apachestatusfixed = apachestatus.toString().replace('●', '').replace('DFOREGROUND', '');
 
     const mariadbstatusfixed = mariadbstatus.toString().replace('●', '');
-    app.get('/status', (req, res) => {
+
+    const getUsersCountAdmin = async (next) => {
+        try {
+            const [rows] = await promisePoolAdmin.execute('SELECT COUNT(*) AS count FROM Users');
+            console.log('count ', rows[0].count);
+            return rows[0].count;
+        } catch (e) {
+            console.error('getUsersCountAdmin', e.message);
+            next(httpError('Database error', 500));
+        }
+    };
+    app.get('/', (req, res) => {
 
         res.render('status', {
             date: date.d,
+            usercount: getUsersCountAdmin,
             mariadbstatus: mariadbstatusfixed,
             apachestatus: apachestatusfixed,
         });
 
-        ;
+
     });
 } else {
     require('./utils/localhost')(app, process.env.HTTP_PORT || 3000);
     const date = { d: Date.now() }
-   
-    app.get('/status', (req, res) => {
-
+    app.get('/', (req, res) => {
         res.render('status', {
             date: date.d,
+            usercount:'no data'
             mariadbstatus: 'no data available. this is localhost',
             apachestatus: 'no data available. this is localhost',
         });
 
-        ;
     });
-
+   
 }
+
 app.use('/users', userRoute );
 // app.use((req, res, next) => {
 //     const err = httpError('Not found', 404);
