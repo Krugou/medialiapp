3a3272045860e561b64da42ee3e5cc63b0e73c3f
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 17:31:03 +0200

    Create errors.js

---
+++ backend-rest/utils/errors.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const httpError = (message, status) => {
+    console.log('error used')
+    const err = new Error(message);
+    err.status = status;
+    return err;
+};
+
+
+module.exports = {
+    httpError,
+}
