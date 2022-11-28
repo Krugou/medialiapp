d62ace56aa3cb042f92cd56001c8f1e59b0aad9e
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-28 17:30:59 +0200

    Update db.js

--- backend-rest/database/db.js
+++ backend-rest/database/db.js
@@ -2,14 +2,32 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const pooladmin = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    user: process.env.DB_USER_ALL,
+    password: process.env.DB_PASS_ALL,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+const poolreguser = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER_CREATEREADUPDATEDELETE,
+    password: process.env.DB_PASS_CREATEREADUPDATEDELETE,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+const pooluser = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER_READ,
+    password: process.env.DB_PASS_READ,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 });
 
-module.exports = pool;
+module.exports = pooladmin, poolreguser, pooluser;
