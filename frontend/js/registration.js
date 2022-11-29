aba8ee3f2d7ab95778d8da5293073eebefbe262b
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2022-11-29 11:54:46 +0200

    Update registration.js

--- frontend/js/registration.js
+++ frontend/js/registration.js
@@ -1,5 +1,5 @@
 'use strict';
-const url = '10.114.34.72/app/'
+const url = '10.114.34.72/app'
 //const url = 'http://localhost:3000'; // change url when uploading to server
 const signupButton = document.querySelector('#signupNappi');
 
