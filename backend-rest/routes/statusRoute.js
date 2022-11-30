1702e3122085110ea4581ecc8bf4fa6e39522fdc
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-30 16:20:35 +0200

    Update statusRoute.js

--- backend-rest/routes/statusRoute.js
+++ backend-rest/routes/statusRoute.js
@@ -11,79 +11,77 @@ const fs = require('fs');
 
 router.get('/', function (req, res, next) {
 
-   
+
 
     if (process.env.NODE_ENV === 'production') {
         const admin = require('../database/db');
-      
+
         const mariadbstatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/mariadbstatus.txt');
         const apachestatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/apachestatus.txt');
-        
+
         const apachestatusfixed = apachestatus.toString().includes("active (running)");
 
 
         const mariadbstatusfixed = mariadbstatus.toString().includes("active (running)");
 
-        
 
-            admin.query(
-                'SELECT * FROM `jakrecipes`.`allthecounts`;',
-                function (err, result) {
-                    if (err) throw err;
 
-                    res.render('status', {
-                        date: date.d,
-                        alluserscount: result[2].count,
-                        usercount: result[1].count,
-                        recipecount: result[0].count,
-                        reciperatingcount: result[8].count,
-                        commentcount: result[3].count,
-                        commentratingcount: result[6].count,
-                        imagecount: result[5].count,
-                        mariadbstatus: mariadbstatusfixed,
-                        apachestatus: apachestatusfixed,
-                    });
-                }
-            );
+        admin.query(
+            'SELECT * FROM `jakrecipes`.`allthecounts`;',
+            function (err, result) {
+                if (err) throw err;
 
-       
+                res.render('status', {
+                    date: date.d,
+                    alluserscount: result[2].count,
+                    usercount: result[1].count,
+                    recipecount: result[0].count,
+                    reciperatingcount: result[8].count,
+                    commentcount: result[3].count,
+                    commentratingcount: result[6].count,
+                    imagecount: result[5].count,
+                    mariadbstatus: mariadbstatusfixed,
+                    apachestatus: apachestatusfixed,
+                });
+            }
+        );
+
+
     } else {
-        
 
-       
-            const local = mysql.createConnection({
-                host: process.env.DB_HOST,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASS,
-                database: process.env.DB_NAME,
-                waitForConnections: true,
-                multipleStatements: true,
-                connectionLimit: 10,
-                queueLimit: 0,
-            });
 
-            local.query(
-                'SELECT * FROM `jakrecipes`.`allthecounts`;',
-                 function (err, result) {
-                    if (err) throw err;
 
-                    res.render('status', {
-                        date: date.d,
-                        alluserscount: result[2].count,
-                        usercount: result[1].count,
-                        recipecount: result[0].count,
-                        reciperatingcount: result[8].count,
-                        commentcount: result[3].count,
-                        commentratingcount: result[6].count,
-                        imagecount: result[5].count,
-                        mariadbstatus: 'no data available. this is localhost',
-                        apachestatus: 'no data available. this is localhost',
-                    })
+        const local = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            waitForConnections: true,
+            multipleStatements: true,
+            connectionLimit: 10,
+            queueLimit: 0,
+        });
 
+        local.query(
+            'SELECT * FROM `jakrecipes`.`allthecounts`;',
+            function (err, result) {
+                if (err) throw err;
+
+                res.render('status', {
+                    date: date.d,
+                    alluserscount: result[2].count,
+                    usercount: result[1].count,
+                    recipecount: result[0].count,
+                    reciperatingcount: result[8].count,
+                    commentcount: result[3].count,
+                    commentratingcount: result[6].count,
+                    imagecount: result[5].count,
+                    mariadbstatus: 'no data available. this is localhost',
+                    apachestatus: 'no data available. this is localhost',
                 })
 
+            })
 
-     
     };
 });
 
