78db9ba3bad63ec0ad740995c669925542314d41
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-30 13:29:25 +0200

    status sivu selkeemmäksi

---
+++ backend-rest/routes/statusRoute.js
@@ -0,0 +1,86 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const router = express.Router();
+
+
+router.get('/', function (req, res, next) {
+
+   
+
+    if (process.env.NODE_ENV === 'production') {
+        const admin = require('../database/db');
+      
+        const mariadbstatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/mariadbstatus.txt');
+        const apachestatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/apachestatus.txt');
+        const date = { d: Date.now() }
+        const apachestatusfixed = apachestatus.toString().includes("active (running)");
+
+
+        const mariadbstatusfixed = mariadbstatus.toString().includes("active (running)");
+
+        
+
+            admin.query(
+                'SELECT * FROM `jakrecipes`.`allthecounts`;',
+                function (err, result) {
+                    if (err) throw err;
+
+                    res.render('status', {
+                        date: date.d,
+                        alluserscount: result[2].count,
+                        usercount: result[1].count,
+                        recipecount: result[0].count,
+                        reciperatingcount: result[8].count,
+                        commentcount: result[3].count,
+                        commentratingcount: result[6].count,
+                        imagecount: result[5].count,
+                        mariadbstatus: mariadbstatusfixed,
+                        apachestatus: apachestatusfixed,
+                    });
+                }
+            );
+
+       
+    } else {
+        
+
+        const date = { d: Date.now() }
+            const local = mysql.createConnection({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASS,
+                database: process.env.DB_NAME,
+                waitForConnections: true,
+                multipleStatements: true,
+                connectionLimit: 10,
+                queueLimit: 0,
+            });
+
+            local.query(
+                'SELECT * FROM `jakrecipes`.`allthecounts`;',
+                [1, 2], function (err, result) {
+                    if (err) throw err;
+
+                    res.render('status', {
+                        date: date.d,
+                        alluserscount: result[2].count,
+                        usercount: result[1].count,
+                        recipecount: result[0].count,
+                        reciperatingcount: result[8].count,
+                        commentcount: result[3].count,
+                        commentratingcount: result[6].count,
+                        imagecount: result[5].count,
+                        mariadbstatus: 'no data available. this is localhost',
+                        apachestatus: 'no data available. this is localhost',
+                    })
+
+                })
+
+
+     
+    };
+});
+
+module.exports = router;
--- backend-rest/thekilledapp.js
+++ backend-rest/thekilledapp.js
@@ -6,6 +6,7 @@ const app = express();
 const fs = require('fs');
 const authRoute = require('./routes/authRoute');
 const userRoute = require('./routes/userRoute');
+const statusRoute = require('./routes/statusRoute');
 const { httpError } = require('./utils/errors');
 
 
@@ -14,122 +15,17 @@ app.use(cors());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 if (process.env.NODE_ENV === 'production') {
-    const admin = require('./database/db');
     require('./utils/production')(app, process.env.HTTP_PORT || 3000, process.env.HTTPS_PORT || 8000);
-    const mariadbstatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/mariadbstatus.txt');
-    const apachestatus = fs.readFileSync('/home/allseeyingeye/medialiapp/backend-rest/apachestatus.txt');
-    const date = { d: Date.now() }
-    const apachestatusfixed = apachestatus.toString().includes("active (running)");
-    
-
-    const mariadbstatusfixed = mariadbstatus.toString().includes("active (running)");
-   
-
-
-    app.get('/status', (req, res) => {
-
-        admin.query(
-            'SELECT * FROM `jakrecipes`.`allthecounts`;',
-             function (err, result) {
-                if (err) throw err;
-                 
-               res.render('status', {
-                    date: date.d,
-                   alluserscount: result[2].count,
-                   usercount: result[1].count,
-                   recipecount: result[0].count,
-                   reciperatingcount: result[8].count,
-                   commentcount: result[3].count,
-                   commentratingcount: result[6].count,
-                   imagecount: result[5].count,
-                    mariadbstatus: mariadbstatusfixed,
-                    apachestatus: apachestatusfixed,
-               });
-            }
-        );
-        // admin.query(
-        //     'SELECT COUNT(*) AS count FROM Users where userrole = 1; SELECT COUNT(*) AS recipes FROM Recipes; SELECT COUNT(*) AS reciperating FROM Reciperating; SELECT COUNT(*) AS comments FROM Comments; SELECT COUNT(*) AS commentratings FROM Commentrating; SELECT COUNT(*) AS images FROM Images;',
-        //     function (err, result) {
-        //         if (err) throw err;
-
-        //         res.render('status', {
-        //             date: date.d,
-        //             usercount: result[0][0].count,
-        //             recipecount: result[1][0].recipes,
-        //             reciperatingcount: result[2][0].reciperating,
-        //             commentcount: result[3][0].comments,
-        //             commentratingcount: result[4][0].commentratings,
-        //             imagecount: result[5][0].images,
-        //             mariadbstatus: mariadbstatusfixed,
-        //             apachestatus: apachestatusfixed,
-        //         });
-        //     }
-        // );
-    });
 } else {
-    const mysql = require('mysql2');
     require('./utils/localhost')(app, process.env.HTTP_PORT || 3000);
-    const date = { d: Date.now() }
-    app.get('/status', (req, res) => {
-        const local = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME,
-            waitForConnections: true,
-            multipleStatements: true,
-            connectionLimit: 10,
-            queueLimit: 0,
-        });
-
-        local.query(
-            'SELECT * FROM `jakrecipes`.`allthecounts`;',
-            [1, 2], function (err, result) {
-                if (err) throw err;
-                
-                res.render('status', {
-                    date: date.d,
-                    alluserscount: result[2].count,
-                    usercount: result[1].count,
-                    recipecount: result[0].count,
-                    reciperatingcount: result[8].count,
-                    commentcount: result[3].count,
-                    commentratingcount: result[6].count,
-                    imagecount: result[5].count,
-                    mariadbstatus: 'no data available. this is localhost',
-                    apachestatus: 'no data available. this is localhost',
-                })
-
-        })
-        // local.query(
-        //     'SELECT COUNT(*) AS count FROM Users where userrole = 1; SELECT COUNT(*) AS recipes FROM Recipes; SELECT COUNT(*) AS reciperating FROM Reciperating; SELECT COUNT(*) AS comments FROM Comments; SELECT COUNT(*) AS commentratings FROM Commentrating; SELECT COUNT(*) AS images FROM Images;',
-        //     [1, 2], function (err, result) {
-        //         if (err) throw err;
-        //         res.render('status', {
-        //             date: date.d,
-        //             usercount: result[0][0].count,
-        //             recipecount: result[1][0].recipes,
-        //             reciperatingcount: result[2][0].reciperating,
-        //             commentcount: result[3][0].comments,
-        //             commentratingcount: result[4][0].commentratings,
-        //             imagecount: result[5][0].images,
-        //             mariadbstatus: 'no data available. this is localhost',
-        //             apachestatus: 'no data available. this is localhost',
-        //         })
-
-        //     })
-
-    }
-    );
 };
 
 
 app.use('/auth', authRoute);
 
 app.use('/users', userRoute);
-
+app.use('/status', statusRoute)
 app.use((req, res, next) => {
     const err = httpError('Not found', 404);
     next(err);
