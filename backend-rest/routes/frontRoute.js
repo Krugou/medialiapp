ef841cdb58bfeefa508ec599ffae53a6b5cd6ab3
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-02 10:20:21 +0200

    Update frontRoute.js

--- backend-rest/routes/frontRoute.js
+++ backend-rest/routes/frontRoute.js
@@ -1,3 +1,8 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+
 router.get('/', function (req, res, next) {
     res.render('front')
 
