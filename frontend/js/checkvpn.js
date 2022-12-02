386b6cc3133720e92a1beb0166a07d992c510cff
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-03 00:32:02 +0200

    Update checkvpn.js

--- frontend/js/checkvpn.js
+++ frontend/js/checkvpn.js
@@ -1,6 +1,6 @@
 // Check if vpn is online to metropolia
 function existsFile(url) {
-    let http = new XMLHttpRequest();
+    var http = new XMLHttpRequest();
     http.open('HEAD', url, false);
     http.send();
     return http.status != 404;
@@ -9,8 +9,8 @@ const checkvalue = existsFile('10.114.34.72');
 
 if (checkvalue === true) {
     var url = 'https://10.114.34.72/app'
-    // console.log('vpn is online');
+    console.log('vpn is online');
 } else {
     var url = 'http://localhost:3000'
-    // console.log('vpn is offline');
+    console.log('vpn is offline');
 }
