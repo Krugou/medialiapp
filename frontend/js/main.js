ff6280a94f060e2505f54571793e14c6e423e99c
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-03 08:31:57 +0200

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,7 @@ const presentationdata = document.getElementById('presentationdata');
 let loadout = ""; 
 function insertPresentationData(amount) {
     for (let i = 0; i < (amount); i++) {
-        loadout += '<figure class="recipefigure"><img src="media/logos/jakrecipeslogo.svg"><p>Nimi</p><p>Aika</p><p>Vaikeus</p><p>Arviot</p><p>Hinta</p><p>Tags</p><button>Katso resepti</button></figure>'
+        loadout += '<figure class="recipefigure"><img src="media/logos/jakrecipeslogo.svg"><p>Nimi</p><p>Aika:'+i+'</p><p>Vaikeus</p><p>Arviot</p><p>Hinta</p><p>Tags</p><button>Katso resepti</button></figure>'
 
     }
     presentationdata.innerHTML = loadout;
@@ -12,4 +12,4 @@ function insertPresentationData(amount) {
 
 // valitse korttien määrä 
 
-insertPresentationData(6);
+insertPresentationData(99);
