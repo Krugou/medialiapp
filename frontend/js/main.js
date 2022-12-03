1b33ab0511cf71dd2f7a5ff546e5a05be8c1ccd2
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-03 08:49:00 +0200

    update fix

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -10,6 +10,15 @@ function insertPresentationData(amount) {
    
 }
 
+function loadDBPresentationData() {
+    const response = await fetch(url + '/recipes', fetchOptions);
+    const json = await response.json();
+    for (let i = 0; i < (json.length); i++) {
+        loadout += '<figure class="recipefigure"><img src="' + json.img[i] + '"><p>' + json.name[i] + '</p><p>' + json.time[i] + '</p><p>' + json.difficulty[i] + '</p><p>' + json.rating[i] + '</p><p>' + json.price[i] + '</p><p>' + json.tags[i]  
+
+    }
+}
+
 // valitse korttien määrä 
 
 insertPresentationData(6);
