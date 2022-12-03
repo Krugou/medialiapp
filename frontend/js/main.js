4bed3d84a4b5cca577c04cfc42c3aa2eb946b47a
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-03 08:27:30 +0200

    presentaatiodata function

---
+++ frontend/js/main.js
@@ -0,0 +1,15 @@
+const presentationdata = document.getElementById('presentationdata');
+
+let loadout = ""; 
+function insertPresentationData(amount) {
+    for (let i = 0; i < (amount); i++) {
+        loadout += '<figure class="recipefigure"><img src="media/logos/jakrecipeslogo.svg"><p>Nimi</p><p>Aika</p><p>Vaikeus</p><p>Arviot</p><p>Hinta</p><p>Tags</p><button>Katso resepti</button></figure>'
+
+    }
+    presentationdata.innerHTML = loadout;
+   
+}
+
+// valitse korttien määrä 
+
+insertPresentationData(6);
