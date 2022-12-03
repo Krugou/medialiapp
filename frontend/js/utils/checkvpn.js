eeb14f1627c619422b21db78e4af746bd9d80c21
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-03 05:21:58 +0200

    url omassa tiedossa
     helposti vaihdettavissa localhostiin js/utils/url.js

--- frontend/js/utils/checkvpn.js
+++ frontend/js/utils/checkvpn.js
@@ -1,19 +1,25 @@
 // Check if vpn is online to metropolia
 function existsFile(url) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.send();
-    console.log('kill xhr')
-    setTimeout(() => {
-        xhr.abort();
-    }, 1000);
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, false);
+  xhr.send();
+
+  if (xhr.status === 200) {
+    console.log('File exists: ' + url);
+
+    return true;
+  } else {
+    console.log('File does not exist: ' + url);
+    xhr.abort();
+    return false;
+  }
 }
 const checkvalue = existsFile('https://10.114.34.72/app/status');
-
 if (checkvalue === true) {
-    var url = 'https://10.114.34.72/app'
-    console.log('vpn is online '+url);
+  var url = 'https://10.114.34.72/app';
+  console.log('vpn is online ' + url);
 } else {
-    var url = 'http://localhost:3000'
-    console.log('vpn is offline '+ url);
+  var url = 'http://localhost:3000';
+  console.log('vpn is offline ' + url);
 }
