524ccbf16f4cf04282f27d6cc9edbca6257d92ef
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-04 10:47:17 +0200

    Update jak-admin.js

--- endback-admin/jak-admin.js
+++ endback-admin/jak-admin.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 } else {
     require('./utils/localhost')(app, process.env.HTTP_PORT || 3001);
 };
-app.get('/', res.send('hello world'));
+app.get('/', function (req, res) { res.send('hello world') });
 app.use('/front', frontRoute);
 app.use('/auth', authRoute);
 
