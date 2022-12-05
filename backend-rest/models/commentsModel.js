4413202ace5b51b841299662f91591d7e8451dfb
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-05 03:43:46 +0200

    Create commentsModel.js

---
+++ backend-rest/models/commentsModel.js
@@ -0,0 +1,56 @@
+const poolRegUser = require('../database/db');
+const promisePoolRegUser = poolRegUser.promise();
+
+const getRecipeCommentsByRecipe = async (params, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(
+            `SELECT CommentText FROM Comments  WHERE CommentRecipe = "${params}";
+                        `);
+        return rows;
+    } catch (e) {
+        console.error('getRecipeComments', e.message);
+        next(httpError('Database error', 500));
+    }
+}
+const getRecipeCommentsByUserId = async (params, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(
+            `SELECT CommentText FROM Comments  WHERE Commentuserid = "${params}";
+                        `);
+        return rows;
+    } catch (e) {
+        console.error('getRecipeComments', e.message);
+        next(httpError('Database error', 500));
+    }
+}
+
+const addComment = async (data, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(`INSERT INTO Comments (CommentText, Commentrecipe, Commentuserid) 
+                                                    VALUES (?, ?, ?);`,
+                                                        data);
+        return rows;
+    } catch (e) {
+        console.error('addComment', e.message);
+        next(httpError('Database error', 500));
+    }
+}
+const deleteCommentById = async (params, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(`DELETE FROM Comments WHERE CommentId = "${params}";
+                        `);
+        return rows;
+    } catch (e) {
+        console.error('deleteCommentById', e.message
+        );
+        next(httpError('Database error', 500));
+    }
+}
+        
+module.exports = {
+    addComment,
+    deleteCommentById,
+    getRecipeCommentsByRecipe,
+    getRecipeCommentsByUserId,
+    
+};
