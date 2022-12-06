c3db6b0bc386138c3b2ea39cd9e7587bf86dd78b
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-06 12:16:51 +0200

    Update layout.js

--- frontend/js/layout.js
+++ frontend/js/layout.js
@@ -46,8 +46,32 @@ newestPresentationData()
 
 // page generation
 
+   // kesken
+function checktoken() {
+        // check sessionStorage
+        if (!sessionStorage.getItem('token') || !sessionStorage.getItem('user')) {
+            location.href = 'login.html';
+            return;
+        }
+        // check if token valid
+        try {
+            const fetchOptions = {
+                headers: {
+                    Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+                },
+            };
+            const response = await fetch(url + '/user/token', fetchOptions);
+            if (!response.ok) {
+                location.href = 'logout.html';
+            } else {
+                const json = await response.json();
+                sessionStorage.setItem('user', JSON.stringify(json.user));
+            }
+        } catch (e) {
+            console.log(e.message);
+        }
 
-   
+}
 
 // frontpage.html alkaa tästä
 const editFilter = document.getElementById("filterModal");
