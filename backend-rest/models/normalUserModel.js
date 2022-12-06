cda77f31b4fe881c6b0c76ae811bc5995c0238bd
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-06 04:37:39 +0200

    Update normalUserModel.js

--- backend-rest/models/normalUserModel.js
+++ backend-rest/models/normalUserModel.js
@@ -16,9 +16,9 @@ const fetchAllRecipes = async (next) => {
 const getAllRecipesMainPage = async (next) => {
     try {
         const [rows] = await promisePoolUser.execute(`SELECT Recipes.Recipeid,Recipes.Recipename , Recipes.Recipetime, Recipes.Recipeguide, Recipes.Recipemaker, Courses.Coursetype, Mealtypes.Mealtype, Images.Imagefilepath
-FROM Recipes INNER JOIN Recipemealtype ON Recipes.Recipeid = Recipemealtype.Recipeid 
-  INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images ON  Recipes.Recipeid = Images.ImageRecipe GROUP BY Recipeid DESC  `);
-        console.log('Someone is at our frontpage');
+FROM Recipes INNER JOIN Recipemealtype  
+  INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images
+  ON  Recipes.Recipeid = Images.ImageRecipe GROUP BY Recipeid DESC ');
         return rows;
     } catch (e) {
         console.error('getAllRecipes', e.message);
