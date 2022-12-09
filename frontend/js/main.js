e7ac4f815d65538559072136131730ddb9d960ad
KaarleH <kaarleh@metropolia.fi>
2022-12-09 12:57:24 +0200

    thumbnail

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,6 +7,23 @@ async function newestPresentationData() {
     const response = await fetch(url + '/recipes/allrecipes/newest');
     const json = await response.json();
 
+
+
+    if (screen.width >= 1000) {
+        json[i].Imagefilepath = 'thumbnails/' + json[i].Imagefilepath + "_500px"
+    }
+
+
+
+    if (screen.width <= 780) {
+        json[i].Imagefilepath = 'thumbnails/' + json[i].Imagefilepath + "_300px"
+    };
+
+
+
+
+
+
      createRecipes(json);
 
 }
