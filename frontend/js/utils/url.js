c67152c0fe7f3cc1ed3dfc2bf366ff08c4302ba8
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-10 02:38:30 +0200

    Update url.js

--- frontend/js/utils/url.js
+++ frontend/js/utils/url.js
@@ -26,8 +26,10 @@
 
 // production
 var url = `https://10.114.34.72/jak`
+var urladmin = `https://10.114.34.72/jak-admin`
 
 
 // development
 // var url = `http://localhost:3000`
+// var urladmin = `http://localhost:3000`
 
