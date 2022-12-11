64428ea96f85209b429476fda0595ad4313bd95a
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-11 04:09:42 +0200

    Update recipesRoute.js

--- backend-rest/routes/recipesRoute.js
+++ backend-rest/routes/recipesRoute.js
@@ -43,13 +43,17 @@ const {
     recipes_mealtypes_get,
     recipe_get,
     comment_post,
-    comment_get, recipe_favorite, recipe_removefavorite,} = require("../controllers/recipesController");
+    comment_get, recipe_favorite, recipe_removefavorite, get_recipes_with_this_coursetype, get_recipes_with_this_mealtype, get_recipes_with_this_low_recipe_price_to_0, } = require("../controllers/recipesController");
 const { body } = require('express-validator');
 router.get('/mealtypes', recipes_mealtypes_get);
 router.get('/newest', getAllNewestRecipesController);
 router.get('/oldest', getAllOldestRecipesController);
 router.get('/:id', recipe_get);
 router.get('/filterrecipes/:recipename', filter_Recipes_By_Recipe_Name);
+router.get('/filtercoursetypes/:coursetype', get_recipes_with_this_coursetype)
+router.get('/filtermealtypes/:mealtypes', get_recipes_with_this_mealtype)
+router.get('/filterbyprice/:lowRecipePrice', get_recipes_with_this_low_recipe_price_to_0)
+
 router.get('/comment/:id',
     body("id").escape(),
     comment_get);
@@ -65,7 +69,7 @@ router.post('/',
 router.post('/comment',
     body("comment").isLength({ min: 1 }).escape(),
     body("recipeid").escape(),
-        comment_post)
+    comment_post)
 router.post('/addfavorite/:id',
     body("id").escape(),
     recipe_favorite)
