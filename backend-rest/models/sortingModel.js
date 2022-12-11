be5391cc6dbf90bcdf669709b97b4388a5c66cec
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-11 03:43:41 +0200

    Update sortingModel.js

--- backend-rest/models/sortingModel.js
+++ backend-rest/models/sortingModel.js
@@ -28,7 +28,7 @@ const getrecipesbylowrecipepriceto0 = async (recipePrice, next) => {
         const [rows] = await promisePoolRegUser.execute(`SELECT Recipes.Recipeid,Recipes.Recipename , Recipes.Recipetime, Recipes.Recipeguide, Recipes.Recipemaker, Courses.Coursetype, Mealtypes.Mealtype, Images.Imagefilepath
 FROM Recipes INNER JOIN Recipemealtype
     INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images
-    ON  Recipes.Recipeid = Images.ImageRecipe WHERE Recipes.Recipeprice >= "${recipePrice}" and Recipes.Recipeprice <= 0  GROUP BY Recipeid DESC`);
+    ON  Recipes.Recipeid = Images.ImageRecipe WHERE Recipes.Recipeprice >= "${recipePrice}" and Recipes.Recipeprice <= 0  GROUP BY Recipeprice DESC`);
         return rows;
     } catch (e) {
         return next(e);
@@ -36,3 +36,21 @@ FROM Recipes INNER JOIN Recipemealtype
 };
 
         
+const getrecipesbyhighrecipepriceto100 = async (recipePrice, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(`SELECT Recipes.Recipeid,Recipes.Recipename , Recipes.Recipetime, Recipes.Recipeguide, Recipes.Recipemaker, Courses.Coursetype, Mealtypes.Mealtype, Images.Imagefilepath
+FROM Recipes INNER JOIN Recipemealtype
+    INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images
+    ON  Recipes.Recipeid = Images.ImageRecipe WHERE Recipes.Recipeprice >= "${recipePrice}" and Recipes.Recipeprice <= 100  GROUP BY Recipeprice DESC`);
+        return rows;
+    } catch (e) {
+        return next(e);
+    }
+};
+
+module.exports = {
+    getrecipesbycoursetype,
+    getrecipesbymealtype,
+    getrecipesbylowrecipepriceto0,
+    getrecipesbyhighrecipepriceto100
+};
