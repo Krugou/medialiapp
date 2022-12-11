2cb93e292d2434bc300112553a5fe40348bb33d6
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-11 03:34:51 +0200

    Create sortingModel.js

---
+++ backend-rest/models/sortingModel.js
@@ -0,0 +1,38 @@
+const poolRegUser = require('../database/db');
+const promisePoolRegUser = poolRegUser.promise();
+
+const getrecipesbycoursetype = async (courseType, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(`SELECT Recipes.Recipeid,Recipes.Recipename , Recipes.Recipetime, Recipes.Recipeguide, Recipes.Recipemaker, Courses.Coursetype, Mealtypes.Mealtype, Images.Imagefilepath
+FROM Recipes INNER JOIN Recipemealtype
+    INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images
+    ON  Recipes.Recipeid = Images.ImageRecipe WHERE Courses.Coursetype = "${courseType}" GROUP BY Recipeid DESC`);
+        return rows;
+    } catch (e) {
+        return next(e);
+    }
+};
+const getrecipesbymealtype = async (mealType, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(`SELECT Recipes.Recipeid,Recipes.Recipename , Recipes.Recipetime, Recipes.Recipeguide, Recipes.Recipemaker, Courses.Coursetype, Mealtypes.Mealtype, Images.Imagefilepath
+FROM Recipes INNER JOIN Recipemealtype
+    INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images
+    ON  Recipes.Recipeid = Images.ImageRecipe WHERE Mealtypes.Mealtype = "${mealType}" GROUP BY Recipeid DESC`);
+        return rows;
+    } catch (e) {
+        return next(e);
+    }
+};
+const getrecipesbylowrecipepriceto0 = async (recipePrice, next) => {
+    try {
+        const [rows] = await promisePoolRegUser.execute(`SELECT Recipes.Recipeid,Recipes.Recipename , Recipes.Recipetime, Recipes.Recipeguide, Recipes.Recipemaker, Courses.Coursetype, Mealtypes.Mealtype, Images.Imagefilepath
+FROM Recipes INNER JOIN Recipemealtype
+    INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images
+    ON  Recipes.Recipeid = Images.ImageRecipe WHERE Recipes.Recipeprice >= "${recipePrice}" and Recipes.Recipeprice <= 0  GROUP BY Recipeid DESC`);
+        return rows;
+    } catch (e) {
+        return next(e);
+    }
+};
+
+        
