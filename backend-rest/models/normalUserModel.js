e933e34b12eaf6c527d4ac804289e686d2b48291
Krugou <krugou@gmail.com>
2022-12-11 12:27:02 +0200

    Update normalUserModel.js

--- backend-rest/models/normalUserModel.js
+++ backend-rest/models/normalUserModel.js
@@ -5,7 +5,7 @@ const httpError = require('http-errors');
 // pääsivu  komento
 const getAllNewestRecipesMainPage = async (next) => {
   try {
-    const [rows] = await promisePoolUser.execute(`SELECT Normaluserview.Recipeid,  Normaluserview.Recipename , Normaluserview.Recipeprice, Normaluserview.Recipetime, Normaluserview.Recipeguide,Normaluserview.Username,Normaluserview.Imagefilepath,Normaluserview.Coursetype,Normaluserview.Mealtype FROM Normaluserview  GROUP BY Normaluserview.Recipename   desc limit 6 `);
+    const [rows] = await promisePoolUser.execute(`SELECT normaluserview.Recipeid,  normaluserview.Recipename , normaluserview.Recipeprice, normaluserview.Recipetime, normaluserview.Recipeguide,normaluserview.Username,normaluserview.Imagefilepath,normaluserview.Coursetype,normaluserview.Mealtype FROM normaluserview  GROUP BY normaluserview.Recipename   desc limit 6 `);
     console.log('Someone is at our frontpage');
     return rows;
   } catch (e) {
@@ -15,7 +15,7 @@ const getAllNewestRecipesMainPage = async (next) => {
 };
 const getAllOldestRecipesMainPage = async (next) => {
   try {
-    const [rows] = await promisePoolUser.execute(`SELECT Normaluserview.Recipeid,  Normaluserview.Recipename , Normaluserview.Recipeprice, Normaluserview.Recipetime, Normaluserview.Recipeguide,Normaluserview.Username,Normaluserview.Imagefilepath,Normaluserview.Coursetype,Normaluserview.Mealtype FROM Normaluserview  GROUP BY Normaluserview.Recipename asc limit 6   `);
+    const [rows] = await promisePoolUser.execute(`SELECT normaluserview.Recipeid,  normaluserview.Recipename , normaluserview.Recipeprice, normaluserview.Recipetime, normaluserview.Recipeguide,normaluserview.Username,normaluserview.Imagefilepath,normaluserview.Coursetype,normaluserview.Mealtype FROM normaluserview  GROUP BY normaluserview.Recipename asc limit 6   `);
     console.log('Someone is at our frontpage');
     return rows;
   } catch (e) {
@@ -27,8 +27,8 @@ const getRecipesByRecipeName = async (recipeName, next) => {
   try {
     console.log('Searched recipe name is: ' + recipeName);
 
-    const [rows] = await promisePoolUser.execute(`SELECT Normaluserview.Recipeid,  Normaluserview.Recipename , Normaluserview.Recipeprice, Normaluserview.Recipetime, Normaluserview.Recipeguide,Normaluserview.Username,Normaluserview.Imagefilepath,Normaluserview.Coursetype,Normaluserview.Mealtype FROM Normaluserview
-                                                        WHERE Normaluserview.Recipename  like "%${recipeName}%" GROUP BY Recipeid DESC`);
+    const [rows] = await promisePoolUser.execute(`SELECT normaluserview.Recipeid,  normaluserview.Recipename , normaluserview.Recipeprice, normaluserview.Recipetime, normaluserview.Recipeguide,normaluserview.Username,normaluserview.Imagefilepath,normaluserview.Coursetype,normaluserview.Mealtype FROM normaluserview
+                                                        WHERE normaluserview.Recipename  like "%${recipeName}%" GROUP BY Recipeid DESC`);
     return rows;
   } catch (e) {
     console.error('Filtering recipes by name', e.message);
