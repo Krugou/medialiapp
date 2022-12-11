200834c6fbb79ebaefd0d7cd34439f8744057a94
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-11 07:42:18 +0200

    Update sortingModel.js

--- backend-rest/models/sortingModel.js
+++ backend-rest/models/sortingModel.js
@@ -14,10 +14,13 @@ FROM Recipes INNER JOIN Recipemealtype
 };
 const getrecipeswiththismealtype = async (mealType, next) => {
   try {
-    const [rows] = await promisePoolRegUser.execute(`SELECT Recipes.Recipeid,Recipes.Recipename , Recipes.Recipetime, Recipes.Recipeguide, Recipes.Recipemaker, Courses.Coursetype, Mealtypes.Mealtype, Images.Imagefilepath
-FROM Recipes INNER JOIN Recipemealtype
-    INNER JOIN Mealtypes  INNER JOIN Courses ON Recipes.Recipecourse = Courses.Courseid   INNER JOIN Images
-    ON  Recipes.Recipeid = Images.ImageRecipe WHERE Mealtypes.Mealtype LIKE "${mealType}%" GROUP BY Recipeid DESC`);
+    const [rows] = await promisePoolRegUser.execute(`SELECT m.'Mealid', m.'Mealtype', r.'Recipeid', r.'Mealid', r1.'Recipeid', r1.'Recipename', r1.'Recipetime', r1.'Recipeguide', r1.'Recipemaker', r1.'Recipecourse', i.'Imageid', i.'Imagefilepath', i.'Imagerecipe', u.'Useremail'
+FROM jakrecipes.mealtypes m 
+	INNER JOIN jakrecipes.recipemealtype r ON ( r.'Mealid' = m.'Mealid'  )  
+	INNER JOIN jakrecipes.recipes r1 ON ( r1.'Recipeid' = r.'Recipeid'  )  
+	INNER JOIN jakrecipes.courses c ON ( c.'Courseid' = r1.'Recipecourse'  )  
+	INNER JOIN jakrecipes.images i ON ( i.'Imagerecipe' = r1.'Recipeid' )  
+	INNER JOIN jakrecipes.users u ON ( u.'Userimg' = i.'Imageid'  )  WHERE Mealtypes.Mealtype LIKE "${mealType}%" GROUP BY Recipeid DESC`);
     return rows;
   } catch (e) {
     return next(e);
