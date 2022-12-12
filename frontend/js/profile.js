f9f6f1de8b4114553f4bdd3a51d86be33d1b927b
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-12 07:55:00 +0200

    profiilitiedot fronttiin

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -13,12 +13,16 @@ fetch(url + '/recipes/reguserprofile/3'/* + userid*/)
         }
     }
     ).then((queryData) => {
+
         createResults(queryData, userposts);
+        getProfileDetails();
     }).catch((error) => {
     }
-);
-function getProfileImage(userid) {
-    fetch(url + '/users/profileimage/3'/* + userid*/)
+    );
+function getProfileDetails(userid) {
+    let Imagefilepath
+    // poista 3 ennen tuotantoon
+    fetch(url + '/users/profiledetails/image/3'/* + userid*/)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -26,14 +30,97 @@ function getProfileImage(userid) {
                 throw 'HTTP ERROR';
             }
         }
-        ).then((Imagefilepath) => {
-            console.log(Imagefilepath)
+        ).then((result) => {
+           
+            
+            const Imagefilepath = result.Imagefilepath;
+            console.log(typeof Imagefilepath)
+            fetch(url + '/users/profiledetails/username/3'/* + userid*/)
+
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw 'HTTP ERROR';
+                    }
+                }
+                ).then((username) => {
+
+                    profiledetails(Imagefilepath, username[0].Username);
+                }).catch((error) => {
+                    console.log(error);
+                }
+                );
+
+
         }
         ).catch((error) => {
+            console.log(error);
         }
 
-    );
+        );
+}
+
+function profiledetails(Imagefilepath, username) {
+    if (Imagefilepath === 'undefined' || Imagefilepath === undefined) {
+        console.log('was it undefined?')
+        Imagefilepath = './media/logos/jakrecipeslogo.svg';
+    }
+    userProfile = document.getElementById('userProfile');
+    const profilePic = document.createElement('IMG');
+    profilePic.setAttribute('alt', 'jakrecipeslogo');
+    profilePic.setAttribute('id', 'profilepic');
+    profilePic.setAttribute('src', Imagefilepath);
+    userProfile.appendChild(profilePic);
+
+    const profileP = document.createElement('P');
+    profileP.setAttribute('class', 'fontsizeforp');
+    userProfile.appendChild(profileP);
+    profileP.innerText = username;
+
+
+    const profileButton = document.createElement('BUTTON');
+    profileButton.setAttribute('id', 'editProfile');
+    userProfile.appendChild(profileButton);
+    profileButton.innerText = 'Muokkaa profiilia';
 }
 
 
+// const noButton = document.getElementById('noButton');
+
+// const editProfile = document.getElementById('editModal');
+
+// const button = document.getElementById('editProfile');
+
+// let closeModal = document.getElementsByClassName('close')[0];
+
+// let closeDelete = document.getElementsByClassName('closeDelete')[0];
+
+// const deleteProfile = document.getElementById('deleteModal');
+
+// const deleteButton = document.getElementById('delete');
+
+// button.onclick = function () {
+//     editProfile.style.display = 'block';
+// };
+
+// closeModal.onclick = function () {
+//     editProfile.style.display = 'none';
+// };
+
+// window.onclick = function (event) {
+//     if (event.target == editProfile) {
+//         editProfile.style.display = 'none';
+//     }
+// };
+// deleteButton.onclick = function () {
+//     deleteProfile.style.display = 'block';
+// };
+// closeDelete.onclick = function () {
+//     deleteProfile.style.display = 'none';
+// };
+
+// noButton.onclick = function () {
+//     deleteProfile.style.display = 'none';
+// };
 
