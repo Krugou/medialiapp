2af41861c3f110e2f624546bdfcb3b6cb2f6a793
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-15 18:04:05 +0200

    admin stuff

--- endback-admin/controllers/authController.js
+++ endback-admin/controllers/authController.js
@@ -30,6 +30,30 @@ const login = (req, res, next) => {
   })(req, res, next);
 };
 
+const check_token = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      next(httpError('Kirjautumisvirhe', 403));
+      res.json({
+        message: 'Kirjautumisvirhe',
+      });
+      return;
+    }
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        next(httpError('Kirjautmisvirhe 2', 403));
+        res.json({
+          message: 'Kirjautumisvirhe',
+        });
+        return;
+      }
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+      return res.json({ user, token });
+    });
+  })(req, res, next);
+};
+
 module.exports = {
   login,
+  check_token,
 };
--- endback-admin/routes/authRoute.js
+++ endback-admin/routes/authRoute.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const {login} = require('../controllers/authController');
-const {body} = require('express-validator');
+const { body } = require('express-validator');
+const { check_token } = require('../controllers/authController');
 
 router.post('/login', login);
+router.get('/token', check_token);
 
 module.exports = router;
