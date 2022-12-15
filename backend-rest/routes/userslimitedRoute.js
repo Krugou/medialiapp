260f0dbc7773bc1e3dfe7627fe35f9ff3512276a
Krugou <59641229+Krugou@users.noreply.github.com>
2022-12-15 17:18:24 +0200

    Update userslimitedRoute.js

--- backend-rest/routes/userslimitedRoute.js
+++ backend-rest/routes/userslimitedRoute.js
@@ -3,13 +3,17 @@ const express = require('express');
 const { user_post, getReg_UserDetails, getReg_UserDetailUsername, getReg_UserDetailImage, get_UserProfile, check_token,
   get_UserProfileLimited, deleteUsersReg_User, putNewProfileDetails
 } = require('../controllers/userController');
+
+
 const { body } = require('express-validator');
 const multer = require('multer');
 const path = require('path');
 const router = express.Router();
 
+// ilman autentikointia (ei tokenia)
 router.get('/profiledetails/image/:userid', getReg_UserDetailImage);
 router.get('/profiledetails/username/:userid', getReg_UserDetailUsername);
 
 
 module.exports = router;
+
