5aaa09c1ba821a93640510987336562076c6973c
Krugou <59641229+Krugou@users.noreply.github.com>
2023-01-03 16:13:38 +0200

    Update signin.js

--- frontend/js/signin.js
+++ frontend/js/signin.js
@@ -6,31 +6,28 @@ signinButton.addEventListener('click', async (evt) => {
   console.log('asd');
   evt.preventDefault();
   const emailInput = document.querySelector('#emailInput').value;
-
+  //const usernameInput = document.querySelector('#usernameInput').value;
   const passwordInput = document.querySelector('#passwordInput').value;
-
   const data = {
     username: emailInput,
     password: passwordInput,
   };
   const fetchOptions = {
-    method: 'GET',
+    method: 'POST',
     headers: {
-      Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+      'Content-Type': 'application/json',
     },
-  }
-  fetch(apiUrl, fetchOptions)
-    .then(response => response.json())
-    .then(data => console.log(data))
+    body: JSON.stringify(data),
+  };
   console.log(data);
   console.log(url);
   const response = await fetch(url + '/auth/login', fetchOptions);
-
   const json = await response.json();
   console.log('login response', json);
   if (!json.user) {
     alert(json.message);
   } else {
+    // save token
     sessionStorage.setItem('token', json.token);
     sessionStorage.setItem('user', JSON.stringify(json.user));
     location.href = 'index.html';
