23251b2ca8bf3345c01e90f8363c576204d185fa
Krugou <59641229+Krugou@users.noreply.github.com>
2023-01-03 16:04:19 +0200

    Update signin.js

--- frontend/js/signin.js
+++ frontend/js/signin.js
@@ -5,25 +5,30 @@ const signinButton = document.querySelector('#signinNappi');
 signinButton.addEventListener('click', async (evt) => {
   console.log('asd');
   evt.preventDefault();
-const emailInput = document.querySelector('#emailInput').value;
+  const emailInput = document.querySelector('#emailInput').value;
 
-const passwordInput = document.querySelector('#passwordInput').value;
+  const passwordInput = document.querySelector('#passwordInput').value;
 
-const data = {
-  username: emailInput,
-  password: passwordInput,
-};
-
-fetch(apiUrl, fetchOptions)
-.then(response => response.json())
-.then(data => console.log(data))
+  const data = {
+    username: emailInput,
+    password: passwordInput,
+  };
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+    },
+  }
+  fetch(apiUrl, fetchOptions)
+    .then(response => response.json())
+    .then(data => console.log(data))
   console.log(data);
   console.log(url);
   const response = await fetch(url + '/auth/login', fetchOptions);
 
   const json = await response.json();
   console.log('login response', json);
- if (!json.user) {
+  if (!json.user) {
     alert(json.message);
   } else {
     sessionStorage.setItem('token', json.token);
