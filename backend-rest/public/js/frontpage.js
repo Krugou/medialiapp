583e2c93bd238f513247442c845a37c9050ae80b
Krugou <59641229+Krugou@users.noreply.github.com>
2023-01-08 15:36:44 +0200

    Revert "siivous"
    This reverts commit 88ec074c82fcc5c58f9fadc231335d31759e3d98.

---
+++ backend-rest/public/js/frontpage.js
@@ -0,0 +1,8 @@
+'use strict';
+const addRecipeButton = document.querySelector('#addrecipesButton');
+
+addRecipeButton.addEventListener('click', async (evt) => {
+  evt.preventDefault();
+  location.href = 'addRecipe';
+
+});
